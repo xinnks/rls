b1ead89b984b41ad87ab1ae7717f495634efe859
James Sinkala <xinnks@gmail.com>
2022-04-24 05:12:10 +0300

    feat(index.js): Add link shortening route

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const { Router } = require('itty-router');
 const { page } = require('./page.js');
+const { getLink, searchLink, saveLink } = require('./db.js');
+const { generateUrl } = require('./utils.js');
+
 // Create a new router
 const router = Router();
 
@@ -25,6 +28,48 @@ router.get("/", () => {
   return rawHtmlResponse(page);
 })
 
+
+/**
+ * This route receives a request containing a full link, creates and returns a json Response containing the short link
+ * @param {Request} {params}
+ * @returns {Response}
+*/
+router.post("/shorten", async request => {
+  let response = {}, link = "";
+  
+  if (request.headers.get("Content-Type") === "application/json") {
+    let res = await request.json();
+    link = res.link;
+  }
+  let exists = await searchLink(link);
+
+  if(exists.state === "success"){
+    response = { 
+      link: exists.body
+    };
+    
+    return new Response(JSON.stringify(response, null, 2), {
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+  }
+  let short = generateUrl(link);
+  let { state, body} = await saveLink(link, short);
+  if(state === "error"){
+    return new Response({error: body.description}, {
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+  }
+  
+  return new Response(JSON.stringify({link: body}, null, 2), {
+    headers: {
+      "Content-Type": "application/json"
+    }
+  })
+})
 addEventListener('fetch', (event) => {
   const { request } = event;
   return event.respondWith(router.handle(request));
